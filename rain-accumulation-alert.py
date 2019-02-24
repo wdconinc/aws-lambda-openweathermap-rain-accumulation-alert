48e1525b58d1aa7521302ffc11037c6e1e301667
Wouter Deconinck <wdconinc@gmail.com>
2019-02-24 18:38:54 -0500

    Send email through SES

--- rain-accumulation-alert.py
+++ rain-accumulation-alert.py
@@ -2,73 +2,116 @@
 # using OpenWeatherMap API, integrate the predicted rain fall,
 # and determine periods of Ghent Dog Park wetness
 
-import boto3, requests, json, smtplib, email
+import boto3, json, smtplib
+from botocore.vendored import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# Connect to AWS SSM Parameter Store
-ssm = boto3.client('ssm', 'us-east-1')
-def get_parameters(name):
-  response = ssm.get_parameters(Names = [name])
-  for parameter in response['Parameters']:
-    return parameter['Value']
+# handler method
+def lambda_handler(event = None, context = None):
 
-# Create a text/plain message
-msg = email.message.EmailMessage()
-msg['Subject'] = 'Ghent Dog Park: Rain Forecast Update'
-msg['From'] = get_parameters("/GhentDogPark/Weather/EmailFrom")
-msg['To'] = get_parameters("/GhentDogPark/Weather/EmailTo")
+  # Connect to AWS SSM Parameter Store
+  ssm = boto3.client('ssm', 'us-east-1')
+  def get_ssm_parameters(name):
+    response = ssm.get_parameters(Names = [name])
+    for parameter in response['Parameters']:
+      return parameter['Value']
 
-# Enter your OpenWeatherMap API key here
-api_key = get_parameters("/GhentDogPark/Weather/APIKey")
+  # Create a text/plain message
+  msg = MIMEMultipart("alternative")
+  msg['Subject'] = 'Ghent Dog Park: Rain Forecast Update'
+  msg['From'] = get_ssm_parameters("/GhentDogPark/Weather/EmailFrom")
+  msg['To'] = get_ssm_parameters("/GhentDogPark/Weather/EmailTo")
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+  # Accumulated rain
+  rain_accumulation = float(get_ssm_parameters("/GhentDogPark/Weather/RainAccumulation"))
+  rain_dryingfactor = float(get_ssm_parameters("/GhentDogPark/Weather/RainDryingFactor"))
+  rain_maxallowed = float(get_ssm_parameters("/GhentDogPark/Weather/RainMaxAllowed"))
 
-# Give city name
-city_name = "Norfolk"
+  # Enter your OpenWeatherMap API key here
+  api_key = get_ssm_parameters("/GhentDogPark/Weather/APIKey")
 
-# complete_url variable to store complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+  # base_url variable to store url
+  base_url = "http://api.openweathermap.org/data/2.5/forecast?"
 
-# get method of requests module return response object
-response = requests.get(complete_url)
+  # Give city name
+  city_name = "Norfolk"
 
-# json method of response object convert json format data into python format data
-r = response.json()
+  # complete_url variable to store complete url address
+  complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-# Start with empty email message
-message = ""
+  # get method of requests module return response object
+  response = requests.get(complete_url)
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to "404",
-# means city is found otherwise, city is not found
-# Check the value of "cod" key is equal to "401",
-# means API key is invalid.
-if r["cod"] != "404" and r["cod"] != "401":
+  # json method of response object convert json format data into python format data
+  r = response.json()
 
-  for x in r["list"]:
+  # Start with empty email message
+  text = "The following rain conditions are expected:\n"
+  html = "<html>\n<head></head>\n<body>The following rain conditions are expected\n"
 
-    description = x["weather"][0]["description"]
+  # Now x contains list of nested dictionaries
+  # "404" means city is not found
+  # "401" means API key is invalid
+  if r["cod"] != "404" and r["cod"] != "401":
 
-    dt = x["dt_txt"]
+    for x in r["list"]:
 
-    if "3h" in x["rain"].keys():
-      rain = x["rain"]["3h"]
-    else:
-      rain = 0
+      desc = x["weather"][0]["description"]
 
-    message += (
-      "\nTime = " + str(dt) + "\n" +
-      "rain (in mm) = " + str(rain) +
-      ", " + str(description) + "\n"
-    )
+      dt = x["dt_txt"]
 
-# Set the message content
-msg.set_content(message)
+      if "3h" in x["rain"].keys():
+        rain = float(x["rain"]["3h"])
+      else:
+        rain = 0
 
-# Print the message
-print(msg)
+      # Accumulate rain
+      rain_accumulation += rain
 
-# Send the message via our own SMTP server
-#s = smtplib.SMTP('localhost')
-#s.send_message(msg)
-#s.quit()
+      # Write line of output
+      text += "At %s: rain = %.2f mm, accum. %.2f mm (%s)\n" % (str(dt), rain, rain_accumulation, desc)
+      html += "<p>At %s: rain = %.2f mm, accum. %.2f mm (%s)</p>\n" % (str(dt), rain, rain_accumulation, desc)
+
+      # Too wet?
+      if rain_accumulation > rain_maxallowed:
+        text += "TOO WET, over %.2f mm\n" % rain_maxallowed
+        html += "<p><b>TOO WET, over %.2f mm</b></p>\n" % rain_maxallowed
+
+      # Drying factor
+      rain_accumulation -= rain_dryingfactor
+      if rain_accumulation < 0:
+        rain_accumulation = 0
+
+  # HTML footer
+  html += "</body>\n</html>\n"
+
+  # Record the MIME types of both parts - text/plain and text/html
+  part1 = MIMEText(text, 'plain')
+  part2 = MIMEText(html, 'html')
+
+  # Attach parts into message container
+  msg.attach(part1)
+  msg.attach(part2)
+
+  # Add attachments
+  attachments = None
+  for attachment in attachments or []:
+    with open(attachment, 'rb') as f:
+      part = MIMEApplication(f.read())
+      part.add_header('Content-Disposition', 'attachment', filename=os.path.basename(attachment))
+      msg.attach(part)
+
+  # Print the message
+  print(msg)
+
+  # Send the message via AWS SES
+  ses = boto3.client('ses', 'us-east-1')
+  response = ses.send_raw_email(
+      Source = msg['From'],
+      Destinations = [msg['To']],
+      RawMessage = {'Data': msg.as_string()}
+  )
+
+if __name__ == "__main__":
+  lambda_handler()
